7b248d6a0c5dccdc350cef1e43137908f36061f6
gujiwuqing <15279279713@163.com>
2024-11-11 21:35:31 +0800

    添加用户模块

--- chat-backend/src/app.module.ts
+++ chat-backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '81.68.168.90',
+      port: 3306,
+      username: 'chat-room',
+      password: '123456',
+      database: 'chat-room',
+      entities: [],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
